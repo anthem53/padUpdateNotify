8c6b3d676de0564f7900f32733ca25aaa7b3723b
anthem <anthem@postech.ac.kr>
2024-11-19 12:41:10 +0900

    feat. add end message

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import sys
 import task
+import atexit
+import log
 
+def endFunction():
+    log.info("프로그램이 특정 이유로 종료 되었습니다.");
+
+atexit.register(endFunction)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2: 
