485d34bbe8abd2fe91253e0fb837674ba7f536ac
anthem <anthem@postech.ac.kr>
2024-12-01 21:03:56 +0900

    feat. event task db 작업중

--- db_event.py
+++ db_event.py
@@ -1,5 +1,6 @@
 import db
 import log
+from datetime import datetime
 
 
 def isExistEvent(eventName):
@@ -9,5 +10,18 @@ def isExistEvent(eventName):
     result = db.fetchall(i)
 
     return len(result) > 0
+
+def insertEvent(event ,curDatetime):
+
+    name = event[0]
+    link = event[1]
+    status = "1"
+    startDate = event[2]
+    endDate = event[3]
+    updateDate= curDatetime
+
+    sql = "INSERT INTO pad_event (event_name, event_link, status, start_date, end_date, update_date) values (%s,%s,%s,%s,%s,%s)" % (name, link, status, startDate,endDate,updateDate)
+    db.execute(sql)
+
 
 
