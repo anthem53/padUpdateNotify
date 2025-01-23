e415f7ba5443322719bc5d2599cd7d06fe5fc8cc
anthem53 <anthem53@postech.ac.kr>
2025-01-23 19:42:45 +0900

    feat. 인터넷 연결 체크 확인

--- statusTest.py
+++ statusTest.py
@@ -1,16 +1,29 @@
 import db
 import crawling
 import log
+import socket
 
 def execute():
     try :
         log.info("서비스 시작 전 설정 테스트 시작합니다.")
+        internetTest()
         db.dbTest()
         crawling.crawlTest()
         log.info("설정 정상 확인")
     except Exception:
         log.error("서비스 설정에 에러가 발생하였습니다.")
         raise Exception
+    
+def internetTest():
+    ipaddress = socket.gethostbyname(socket.gethostname())
+    try : 
+        if ipaddress == '127.0.0.1':
+            raise Exception("Disconnect Internet")
+        else:
+            log.info("인터넷이 정상적으로 연결 되었습니다.")
+    except Exception :
+        raise Exception
+
 
 if __name__ == "__main__":
     execute()
