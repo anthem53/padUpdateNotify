6e9e6c0ebbd9bd232b1d2aa7a4d4df5f6151727f
anthem53 <anthem53@postech.ac.kr>
2025-02-22 20:33:28 +0900

    fix. 문구 수정

--- statusTest.py
+++ statusTest.py
@@ -9,7 +9,7 @@ def execute():
         internetTest()
         db.dbTest()
         crawling.crawlTest()
-        log.info("설정 정상 확인")
+        log.info("서비스 시작 전 설정 테스트 성공")
     except Exception:
         log.error("서비스 설정에 에러가 발생하였습니다.")
         raise Exception
